4b64d93c9fcc1c3e2d647a3eec534c83cd4070fe
Allene Norton <norton.allene@gmail.com>
2020-06-29 02:11:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   nuSentence = sentence.split
   nuSentence2 = []
-  nuSentence.each do |x|
+  nuSentence.collect do |x|
     nuSentence2 << x.reverse + " "
   end
   revSentence = nuSentence2.join
